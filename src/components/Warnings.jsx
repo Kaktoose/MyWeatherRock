ecc92ffcb2b959e04c48944311b2e6c55dd22ffb
Jack Ibsen <jackibsen@Jack-Ibsen-2023.teamspace.ad>
2023-12-13 14:22:55 -0400

    added rain animations and weather warnings

---
+++ src/components/Warnings.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import { keys } from "../keys.js";
+
+const Warnings = (props) =>{
+
+
+
+
+   
+
+    return (
+        <>
+            <h2>{props.warningName}</h2>
+        
+        
+        </>
+    )
+  
+
+
+}
+
+
+
+export default Warnings;
+
+
+
+
+
--- src/components/WeatherEnvironment.jsx
+++ src/components/WeatherEnvironment.jsx
@@ -3,12 +3,13 @@ import CurrentConditions from "./CurrentConditions";
 import getSeason from "../seasons";
 import { useState, useEffect } from 'react'
 
-
 const rockConditions = {
     'rain': '/assets/rock-rain.png',
     'dry': '/assets/rock-base.png',
     'snow': '/assets/rock-snow.png',
-    'lightning': '/assets/rock-lightning.png'
+    'lightning': '/assets/rock-lightning.png',
+    'rainfallWarning': '/assets/warning/rain.png',
+    'snowWarning': '/assets/warning/snowWarning.png'
 }
 
 
@@ -16,8 +17,9 @@ const rockBackground = (season, dayNight)  => {
     return{
     'clear': `/assets/atmosphere/${season}/clear${dayNight}.png`,
     'cloudy': `/assets/atmosphere/${season}/cloudy${dayNight}.png`,
-    'fog': `/assets/atmosphere/${season}/fog${dayNight}.png`,
-    'rainy': `/assets/atmosphere/${season}/raining${dayNight}.png`
+    'rainy': `/assets/atmosphere/${season}/raining${dayNight}.png`,
+    'fog': `/assets/atmosphere/${season}/fog${dayNight}.png`
+    
     }
 
 
@@ -30,6 +32,7 @@ const rockBackground = (season, dayNight)  => {
 
 const WeatherEnvironment = (props) => {
     let season = getSeason(props.latitude, props.temp);
+    console.log('warning', props.warning)
     
    if(props.temp > 9){
         season = 'Spring'
@@ -45,72 +48,83 @@ const WeatherEnvironment = (props) => {
     const [rockStatus, setRockStatus] = useState()
     const [message, setMessage] = useState()
     const [isSnowing, setIsSnowing] = useState(false)
-    //const [isRaining, setIsRaining] = useState(false)
+    const [isRaining, setIsRaining] = useState(false)
     //const [isLightning, setIsLightning] = useState(false)
-
     let clouds = ["Partly cloudy",  "Cloudy",  "Overcast"]
-    let rain = ["Light drizzle", "Patchy light drizzle", "Patchy rain possible", "Patchy light rain", "Light rain", "Moderate rain at times", "Moderate rain", "Heavy rain at times", "Heavy rain", "Light rain shower", "Moderate or heavy rain shower", "Torrential rain shower"]
-    let snow =["Snow" || "Blowing Snow" || "Blizzard", "Light snow", "Patchy light snow", "Patchy moderate snow", "Moderate snow", "Patchy heavy snow", "Heavy snow", "Light snow showers", "Moderate or heavy snow showers", "Patchy freezing drizzle possible", "Freezing drizzle", "Heavy freezing drizzle", "Light Freezing Rain", "Moderate or heavy freezing rain"]
+    let rain = ["Patchy light drizzle", "Light drizzle",  "Patchy rain possible", "Patchy light rain", "Light rain", "Moderate rain at times", "Moderate rain", "Heavy rain at times", "Heavy rain", "Light rain shower", "Moderate or heavy rain shower", "Torrential rain shower", "Moderate or heavy sleet", "Light sleet showers", "Patchy sleet possible",]
+    let snow =["Snow" || "Blowing Snow" || "Blizzard", "Light snow", "Patchy light snow", "Patchy moderate snow", "Moderate snow", "Patchy heavy snow", "Heavy snow", "Light snow showers", "Moderate or heavy snow showers", "Patchy freezing drizzle possible", "Freezing drizzle", "Heavy freezing drizzle", "Light Freezing Rain", "Moderate or heavy freezing rain",  "Ice pellets", "Light showers of ice pellets", "Moderate or heavy showers of ice pellets" ]
     let thunder = ["Patchy light snow with thunder", "Moderate or heavy snow with thunder", "Patchy light rain with thunder" ]
     let clear = ["Sunny", "Clear"]
-    let fog = ["Fog", "Mist"]
+    let fog = ["Fog", "Mist", "Freezing fog"]
     let snowThunder = ["Patchy light snow with thunder", "Moderate or heavy snow with thunder"]
     let rainThunder = ["Moderate or heavy rain with thunder", "Thundery outbreaks possible"]
+   
+    console.log('-------', rain.includes(props.condition))
+
     useEffect( () =>{
         const dayNight = props.isDay ? "Day" : "Night"
         console.log('-----', dayNight)
         setIsSnowing(false)
+
+        
+        
         if(clouds.includes(props.condition)){
             console.log('theres clourds')
             
-
+            
             setBackground(rockBackground(season, dayNight).cloudy)
             setRockStatus(rockConditions.dry)
             setMessage(`"Your rock has taken up cloud watching, almost as boring as being a rock."`)
-
+            setIsRaining(false)
         } else if(rain.includes(props.condition)){
-            console.log('theres clourds')
-
+            console.log('theres rain')
+            
             setBackground(rockBackground(season, dayNight).rainy)
+            setMessage(`"Your rock has taken to sitting in the rain, I guess it's just their thing."`)
             setRockStatus(rockConditions.rain)
-            setMessage(`"Your rock has taken up cloud watching, almost as boring as being a rock."`)
-
+            setIsRaining(true)
         } else if(clear.includes(props.condition)){
             console.log('theres sun')
             setBackground(rockBackground(season, dayNight).clear)
             setRockStatus(rockConditions.dry)
-            setMessage(`"The heat from the sun beams down on your rock, it reminds them of the good old days being formed by heat and pressure underground. Much more interesting then sitting here all day."`)
-
+            setMessage(`"It would be a shame to take this clear day for granite"`)
+            setIsRaining(false)
         } else if(fog.includes(props.condition)){
             console.log('theres clourds')
-
+            
             setBackground(rockBackground(season, dayNight).fog)
             setRockStatus(rockConditions.dry)
-            setMessage(`"Your rock has taken up cloud watching, almost as boring as being a rock."`)
-
+            setMessage(`"Your rock sits in the fog, it has absolutely zero effect on it's life."`)
+            setIsRaining(false)
         } else if(snow.includes(props.condition)){
             setBackground(rockBackground(season, dayNight).cloudy)
             setRockStatus(rockConditions.snow)
-            setMessage('snow')
+            setMessage('Your rock fears of becoming the eye of a snowman')
             setIsSnowing(true)
+            setIsRaining(false)
         } else if(snowThunder.includes(props.condition)){
             setBackground(rockBackground(season, dayNight).cloudy)
-            
+            setIsSnowing(true)            
             setRockStatus(rockConditions.lightning)
-            setMessage('your rock got struck by lightning, womp womp')
-
-
-
+            setMessage(`"Your rock has managed to get struck by lightning in the snow. That's quite something.`)
+            setIsRaining(false)
+            
+            
         } else if(rainThunder.includes(props.condition)){
             
             setBackground(rockBackground(season, dayNight).rainy)
-            
+            setIsRaining(true)        
             setRockStatus(rockConditions.lightning)
             
-            setMessage('your rock got struck by lightning, womp womp')
-
+            setMessage('Your rock was burnt to a crisp by lightning. Womp Womp :/')
         }
         
+        if(props.warning.includes('RAIN') || props.warning.includes('FLOOD') ){
+            setRockStatus(rockConditions.rainfallWarning)
+        
+        }  else if(props.warning.includes('SNOW') || props.warning.includes('BLIZZARD') || props.warning.includes('WINTER') ){
+            setRockStatus(rockConditions.snowWarning)
+        }
         
         /*
         switch (props.condition) {
@@ -165,6 +179,7 @@ const WeatherEnvironment = (props) => {
                     
             }*/ 
             console.log("condition  ", props.condition)
+            console.log('warnings', props.warning)
         }, [season, props])
     
     return (
@@ -173,7 +188,7 @@ const WeatherEnvironment = (props) => {
             <div style={{ marginBottom: "0" }} className="weatherEnvironment" id="environment">
                 <img className="rock" src={rockStatus} />
                 {isSnowing && <div className="snow"></div> }
-                
+                {isRaining && <div className="rain"></div>}                
                 <img className="background" src={background} />
 
             </div>

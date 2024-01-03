9e95187e8a98bd531cc42e6b10e24bc2e4856417
Jack Ibsen <jackibsen@Jack-Ibsen-2023.teamspace.ad>
2024-01-03 15:23:14 -0400

    added version number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () =>{
         <footer className="Footer">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         
-            <a class="fa fa-github" style={{ "color": "aliceBlue", 'margin': "10px"}}><a style={{'fontSize': 'min(max(2vw, 2vw), 30px)', 'textDecoration': "none", 'color': 'aliceBlue'}} href="https://github.com/Kaktoose/MyWeatherRock/">&#32; GitHub</a></a>
-            
-            <a>© 2023 MyWeatherRock</a>
+            <span>V1.0.0</span>
+            <span>© 2023 MyWeatherRock</span>
+            <span class="fa fa-github" style={{ "color": "aliceBlue", 'margin': "10px"}}><a style={{'fontSize': 'min(max(2vw, 2vw), 30px)', 'textDecoration': "none", 'color': 'aliceBlue'}} href="https://github.com/Kaktoose/MyWeatherRock/">&#32; GitHub</a></span>
 
 
 
